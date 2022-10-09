a5aa249d0dc8fb86de32535d8e8799ea6838755b
DanilaDashevsky <99722380+DanilaDashevsky@users.noreply.github.com>
2022-10-09 19:44:53 +0300

    NewChenge

--- LessonTask/LessonTask.cpp
+++ LessonTask/LessonTask.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 #include <Windows.h>
 using namespace std;
 struct Student
@@ -17,43 +19,72 @@ int main()
     try {
         SetConsoleCP(1251);
         SetConsoleOutputCP(1251);
-        const int size_group = 2;
+        const int size_group = 3; // объём массива
         int k = 0;
-        int error_num = 1;
+        int depend=0;
         Student main_group[size_group] = {}; // основная группа студентов
         Student bad_student[size_group] = {}; // плохая группа студентов
         std::cout << "Здравствуйте, введите данные студентов:\n";
-        for (char i = 0; i < size_group; i++)
+        std::cout << "Предупреждение:При вводе данных не используйте пробелы!\n";
+        for (char i = 0; i < size_group; i++) // заполнение массива объектов структур
         {
-            std::cout << "Введите имя с инициалами:";
-            std::cin >> main_group[i].Name;
-            if (main_group[i].Name.find('.') == -1)
-                throw (5);
+            do {
+                if (depend >= 1)
+                    cout << "Введено некорректное значение! Добавьте точки или уберите пробелы.\n";
+            std::cout << "Введите имя с инициалами(Петров.А.А):";
+                std::cin >> main_group[i].Name;
+                depend += 1;
+            } while (main_group[i].Name.find('.') == -1);
+            depend = 0;
+            do{
+                if (depend >= 1)
+                    cout << "Введено некорректное значение!\n";
             std::cout << "Введите номер группы студента:";
             std::cin >> main_group[i].number;
-            if (main_group[i].number == 0)
-                throw (5);
-            std::cout << "Введите первую оценку:";
-            std::cin >> main_group[i].education[0];
-            if (main_group[i].education[0] == 0 || main_group[i].education[0] > 5 || main_group[i].education[0] < 2)
-                throw (5);
-            std::cout << "Введите вторую оценку:";
-            std::cin >> main_group[i].education[1];
-            if (main_group[i].education[1] == 0 || main_group[i].education[1] > 5 || main_group[i].education[1] < 2)
-                throw (5);
-            std::cout << "Введите 3-ю оценку:";
-            std::cin >> main_group[i].education[2];
-            if (main_group[i].education[2] == 0 || main_group[i].education[2] > 5 || main_group[i].education[2] < 2)
-                throw (5);
-            std::cout << "Введите 4-ю оценку:";
-            std::cin >> main_group[i].education[3];
-            if (main_group[i].education[3] == 0 || main_group[i].education[3] > 5 || main_group[i].education[3] < 2)
-                throw (5);
-            std::cout << "Введиет 5-ю оценку:";
-            std::cin >> main_group[i].education[4];
-            if (main_group[i].education[4] == 0 || main_group[i].education[4] > 5 || main_group[i].education[4] < 2)
-                throw (5);
-            if (main_group[i].education[0] == 2 || main_group[i].education[1] == 2 || main_group[i].education[2] == 2 || main_group[i].education[3] == 2 || main_group[i].education[4] == 2)
+            depend += 1;
+            } while (main_group[i].number == 0);
+            depend = 0;
+            do {
+                if (depend >= 1)
+                    cout << "Введено некорректное значение! Оценка не должна быть больше 5 и меньше 2\n";
+                std::cout << "Введите 1-ю оценку:";
+                std::cin >> main_group[i].education[0];
+                depend += 1;
+            } while (main_group[i].education[0] > 5 || main_group[i].education[0] < 2);
+            depend = 0;
+            do {
+                if (depend >= 1)
+                    cout << "Введено некорректное значение! Оценка не должна быть больше 5 и меньше 2\n";
+                std::cout << "Введите 2-ю оценку:";
+                std::cin >> main_group[i].education[1];
+                depend += 1;
+            } while (main_group[i].education[1] > 5 || main_group[i].education[1] < 2);
+            depend = 0;
+            do {
+                if (depend >= 1)
+                    cout << "Введено некорректное значение! Оценка не должна быть больше 5 и меньше 2\n";
+                std::cout << "Введите 3-ю оценку:";
+                std::cin >> main_group[i].education[2];
+                depend += 1;
+            } while ( main_group[i].education[2] > 5 || main_group[i].education[2] < 2);
+            depend = 0;
+            do {
+                if (depend >= 1)
+                    cout << "Введено некорректное значение! Оценка не должна быть больше 5 и меньше 2\n";
+                std::cout << "Введите 4-ю оценку:";
+                std::cin >> main_group[i].education[3];
+                depend += 1;
+            } while (main_group[i].education[3] > 5 || main_group[i].education[3] < 2);
+            depend = 0;
+            do {
+                if (depend >= 1)
+                    cout << "Введено некорректное значение! Оценка не должна быть больше 5 и меньше 2\n";
+                std::cout << "Введиет 5-ю оценку:";
+                std::cin >> main_group[i].education[4];
+                depend += 1;
+            } while (main_group[i].education[4] > 5 || main_group[i].education[4] < 2);
+            depend = 0;
+            if (main_group[i].education[0] == 2 || main_group[i].education[1] == 2 || main_group[i].education[2] == 2 || main_group[i].education[3] == 2 || main_group[i].education[4] == 2) // отделение нормальных от плохих
             {
                 bad_student[k].Name = main_group[i].Name;
                 bad_student[k].number = main_group[i].number;
@@ -68,17 +99,18 @@ int main()
         }
         if (k > 0)
         {
-            std::cout << "Призренные мира сего:\n";
+            std::cout << "Призренные мира сего:\n"; // вывод плохих студентов
             for (int i = 0; i < k; i++)
-                std::cout << bad_student[i].Name << " " << bad_student[i].number;
+            {
+                std::cout << bad_student[i].Name << " " << bad_student[i].number << "\n";
+            }
         }
         else
-            std::cout << "Двоячников нет!";
+            cout << "Двоячников нет!";
     }
-    catch (int Mistake)
+    catch (int Mistake) // защита на некорректный ввод
     {
         std::cout << "Введено некорректное значение!";
-        //break;
     }
 }
 
